d86790bd1ade3eabb638773da9cc3501666f6a0e
minageorge <minageorge888@gmail.com>
2018-06-17 13:16:58 +0200

    .

---
+++ app/src/main/java/swap/go/george/mina/goswap/rest/Client.java
@@ -0,0 +1,32 @@
+package swap.go.george.mina.goswap.rest;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import swap.go.george.mina.goswap.rest.apiModel.Category;
+import swap.go.george.mina.goswap.rest.apiModel.Governate;
+import swap.go.george.mina.goswap.rest.apiModel.SignUpInfo;
+
+public interface Client {
+
+    @GET("all_items-for-swap/")
+    Call<ArrayList<Category>> getAllItems();
+
+
+    @GET("governate_items-for-swap/{goverquery}/")
+    Call<ArrayList<Category>> getAllItemsByGovernate(@Path("goverquery") String query);
+
+    @GET("city_items-for-swap/{cityquery}/")
+    Call<ArrayList<Category>> getAllItemsByCity(@Path("cityquery") String query);
+
+    @GET("governates/")
+    Call<ArrayList<Governate>> getGovernates();
+
+    @GET("signup_fb/{name}/{email}/{fb_id}/")
+    Call<ArrayList<SignUpInfo>> signUpUsingFb(@Path("name") String namee,
+                                              @Path("email") String emaill,
+                                              @Path("fb_id") String picc);
+}
